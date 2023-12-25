32d3e41a93eb315916d710cc184892a8acda75ca
Nurhadi Sasono <nurhadisasono002@gmail.com>
2023-12-25 22:20:56 +0800

    penambahan crud bidang dan bidang anggaran

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BidangController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TahunAnggaranController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +17,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/admin/dashboard', 'App\Http\Controllers\DashboardController@index')->name('admin.dashboard');
-
 Route::get('/admin/tahun-anggaran', 'App\Http\Controllers\TahunAnggaranController@index')->name('admin.tahun_anggaran');
 Route::get('/admin/tahun-anggaran/create', 'App\Http\Controllers\TahunAnggaranController@create')->name('admin.tahun_anggaran.create');
 Route::post('/admin/tahun-anggaran', 'App\Http\Controllers\TahunAnggaranController@add')->name('admin.tahun_anggaran.add');
 Route::put('/admin/tahun-anggaran', 'App\Http\Controllers\TahunAnggaranController@update')->name('admin.tahun_anggaran.update');
+Route::get('/admin/bidang', 'App\Http\Controllers\BidangController@index')->name('admin.bidang');
+Route::get('/admin/bidang/create', 'App\Http\Controllers\BidangController@create')->name('admin.bidang.create');
+Route::post('/admin/bidang', 'App\Http\Controllers\BidangController@add')->name('admin.bidang.add');
+Route::put('/admin/bidang', 'App\Http\Controllers\BidangController@update')->name('admin.bidang.update');
+Route::delete('/admin/bidang', 'App\Http\Controllers\BidangController@delete')->name('admin.bidang.delete');
+Route::get('/admin/bidang-anggaran', 'App\Http\Controllers\BidangAnggaranController@index')->name('admin.bidang_anggaran');
+Route::get('/admin/bidang-anggaran/create', 'App\Http\Controllers\BidangAnggaranController@create')->name('admin.bidang_anggaran.create');
+Route::post('/admin/bidang-anggaran', 'App\Http\Controllers\BidangAnggaranController@add')->name('admin.bidang_anggaran.add');
+Route::put('/admin/bidang-anggaran', 'App\Http\Controllers\BidangAnggaranController@update')->name('admin.bidang_anggaran.update');
+Route::delete('/admin/bidang-anggaran', 'App\Http\Controllers\BidangAnggaranController@delete')->name('admin.bidang_anggaran.delete');
