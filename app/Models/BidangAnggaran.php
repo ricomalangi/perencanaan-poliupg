3af8b86a87de22364e628cc2e2021825fa319e8f
ricomalangi <rikomalangi@gmail.com>
2023-12-26 13:06:33 +0800

    add model bidang and bidang anggaran

---
+++ app/Models/BidangAnggaran.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BidangAnggaran extends Model
+{
+    use HasFactory;
+    protected $table = 'tb_bidang_anggaran';
+}
